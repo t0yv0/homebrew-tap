492fa6309861fc9984976419631befa191a57241
Anton Tayanovskyy <anton@pulumi.com>
2022-05-04 13:40:01 -0400

    Disable checksums

--- Formula/pulumi2.rb
+++ Formula/pulumi2.rb
@@ -7,14 +7,14 @@ class Pulumi2 < Formula
   license "Apache-2.0"
   head "https://github.com/t0yv0/pulumi.git", branch: "master"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b2b04612f847e00d0c5ae52c80053028936ee5c5f2a19c6d9c50d9fc8766b5ec"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4771d8f96f6b15e4b5aed84c0b7c4d0fdc6712f8c59f478b973dfd3149b94f06"
-    sha256 cellar: :any_skip_relocation, monterey:       "8c2d302507c1860fc93a812c1a7d79ebbc4e4fc88e86715e298c4fd6bd29b518"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8314e361ece3bba9e7429376d619995f42be3450586ed62b367a94f139ffe98a"
-    sha256 cellar: :any_skip_relocation, catalina:       "37999cad25dee32e6db885042c2404d09a4df8d1066e3ae5889e619f665d0247"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "622785e91cd64510e39680dd3db941689d5454aec45963a7d7b1d33b40dcef71"
-  end
+  # bottle do
+  #   sha256 cellar: :any_skip_relocation, arm64_monterey: "b2b04612f847e00d0c5ae52c80053028936ee5c5f2a19c6d9c50d9fc8766b5ec"
+  #   sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4771d8f96f6b15e4b5aed84c0b7c4d0fdc6712f8c59f478b973dfd3149b94f06"
+  #   sha256 cellar: :any_skip_relocation, monterey:       "8c2d302507c1860fc93a812c1a7d79ebbc4e4fc88e86715e298c4fd6bd29b518"
+  #   sha256 cellar: :any_skip_relocation, big_sur:        "8314e361ece3bba9e7429376d619995f42be3450586ed62b367a94f139ffe98a"
+  #   sha256 cellar: :any_skip_relocation, catalina:       "37999cad25dee32e6db885042c2404d09a4df8d1066e3ae5889e619f665d0247"
+  #   sha256 cellar: :any_skip_relocation, x86_64_linux:   "622785e91cd64510e39680dd3db941689d5454aec45963a7d7b1d33b40dcef71"
+  # end
 
   depends_on "go" => :build
 
